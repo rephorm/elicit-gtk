5f35441eda3dc62d83a0aad34c35b96bf520dcb0
Brian Mattern <rephorm@rephorm.com>
2010-03-07 18:23:13 -0800

    loading non-existant palette doesn't raise exception
    instead, this just sets the filename to save to
    also, fix saving to be correct

--- src/palette.py
+++ src/palette.py
@@ -1,4 +1,5 @@
 from color import Color
+import os
 
 class Palette:
   def __init__(self):
@@ -21,48 +22,62 @@ class Palette:
     self.colors[0:0] = color
 
   def load(self, filename):
-    with open(filename) as f:
-      self.filename = filename
-      self.colors = []
-      self.load_errors = []
+    self.colors = []
+    self.load_errors = []
+    self.filename = filename
 
-      # Check that file is a palette file
-      if f.readline().strip() != 'GIMP Palette':
-        self.load_errors.append("Invalid file format.".format(filename))
-        return False
+    try:
+      with open(filename) as f:
+        # Check that file is a palette file
+        if f.readline().strip() != 'GIMP Palette':
+          self.load_errors.append("Invalid file format.".format(filename))
+          return False
 
-      lineno = 0
-      for line in f:
-        lineno += 1
-        if line[0] == '#': continue
-        elif line[0:5] == 'Name:':
-          self.name = line[5:].strip()
-        elif line[0:8] == 'Columns:':
-          try:
-            self.columns = int(line[8:].strip())
-          except ValueError:
-            self.load_errors.append("Columns value (line {0}) must be integer. Using default value.".format(lineno))
-            self.columns = 0
-        else:
-          try:
-            r = int(line[0:3])
-            g = int(line[4:7])
-            b = int(line[8:11])
-            cname = line[12:].strip()
+        lineno = 0
+        for line in f:
+          lineno += 1
+          if line[0] == '#': continue
+          elif line[0:5] == 'Name:':
+            self.name = line[5:].strip()
+          elif line[0:8] == 'Columns:':
+            try:
+              self.columns = int(line[8:].strip())
+            except ValueError:
+              self.load_errors.append("Columns value (line {0}) must be integer. Using default value.".format(lineno))
+              self.columns = 0
+          else:
+            try:
+              r = int(line[0:3])
+              g = int(line[4:7])
+              b = int(line[8:11])
+              cname = line[12:].strip()
 
-            c = Color()
-            c.name = cname
-            c.set_rgb(r,g,b)
-            self.colors.append(c)
-          except:
-            self.load_errors.append("Invalid color entry on line {0}. Skipping.\n".format(lineno))
+              c = Color()
+              c.name = cname
+              c.set_rgb(r,g,b)
+              self.colors.append(c)
+            except:
+              self.load_errors.append("Invalid color entry on line {0}. Skipping.\n".format(lineno))
+    except IOError:
+        #if nonexistant filename is passed in, assume we want to save to that in the future
+      pass
     return (len(self.load_errors) == 0)
      
-  def save(self, filename):
-    with open(filename, 'w'):
-      write("GIMP Palette\n")
-      write("Name: %s" % self.name)
-      write("Columns: %d" % self.columns)
+  def save(self, filename = None):
+    if filename:
+      self.filename = filename
+    if self.filename == None:
+      raise "No filename specified."
+
+    dir = os.path.dirname(self.filename)
+    if not os.path.exists(dir):
+      os.makedirs(dir)
+
+    with open(self.filename, 'w') as f:
+      f.write("GIMP Palette\n")
+      f.write("Name: %s\n" % self.name)
+      f.write("Columns: %d\n" % self.columns)
+      f.write("#\n")
       for c in self.colors:
-        write("% 3d % 3d % 3d\t%s" % (c.r, c.g, c.b, c.name))
+        f.write("%3d %3d %3d\t%s\n" % (c.r, c.g, c.b, c.name))
 
