b05f736ad90e4457afddd67684ed24b44ba256fd
Brian Mattern <rephorm@rephorm.com>
2010-03-12 06:04:51 -0800

    add palette thumbnail to dropdown

--- src/palette_tools.py
+++ src/palette_tools.py
@@ -7,14 +7,57 @@ from palette import Palette
 
 class PaletteList(gtk.ListStore):
   def __init__(self):
-    super(PaletteList,self).__init__(gobject.TYPE_STRING, gobject.TYPE_STRING, Palette)
+    super(PaletteList,self).__init__(gobject.TYPE_STRING, gobject.TYPE_STRING, Palette, gtk.gdk.Pixbuf)
+
+  def generate_pixbuf(self, palette):
+    num = len(palette.colors)
+    w = 32
+    if num < 8:
+      sw = 32
+      sh = 32 / num
+      h = sh * num
+    elif num < 16:
+      sw = 16
+      sh = 32 / (num / 2)
+      h = sh * num / 2
+    elif num < 32:
+      sw = 4
+      sh = 32 / (num / 4)
+      h = sh * num / 4
+    elif num < 64:
+      sw = 4
+      sh = 32 / (num / 8)
+      h = sh * num / 8
+    else:
+      sw = 4
+      sh = 4
+      h = 32
+
+    pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, 0, 8, w, h)
+    pixels = pb.get_pixels_array()
+
+    for x in range(0,w):
+      for y in range(0,h):
+        #border
+        if x == 0 or y == 0 or x == w-1 or y == h -1:
+          pixels[y,x] = (64,64,64)
+          continue
+
+        i = (w / sw) * (y/sh) + (x/sw)
+        if i < num:
+          pixels[y,x] = palette.colors[i].rgb()
+        else:
+          pixels[y,x] = (255,255,255)
+
+    return pb
 
   def load(self, dir):
     for f in glob.glob(os.path.join(dir,'*.gpl')):
       self.append_path(f)
 
   def append(self, palette):
-    super(PaletteList,self).append((palette.name, palette.filename, palette))
+    palette.connect('changed', self.palette_changed)
+    super(PaletteList,self).append((palette.name, palette.filename, palette, self.generate_pixbuf(palette)))
 
   def append_path(self, path):
     p = Palette()
@@ -39,6 +82,11 @@ class PaletteList(gtk.ListStore):
         return index
       index += 1
 
+  def palette_changed(self, palette):
+    index = self.index_of_palette(palette)
+    self[index][3] = self.generate_pixbuf(palette)
+
+
 class PaletteCombo(gtk.ComboBoxEntry):
   __gsignals__ = {
     'selected': (gobject.SIGNAL_RUN_FIRST, gobject.TYPE_NONE, (Palette,))
@@ -48,6 +96,10 @@ class PaletteCombo(gtk.ComboBoxEntry):
 
     self.set_text_column(0)
     self.active = -1
+
+    cell = gtk.CellRendererPixbuf()
+    self.pack_start(cell, False)
+    self.add_attribute(cell, 'pixbuf', 3)
 
     self.connect('changed', self.changed)
 
