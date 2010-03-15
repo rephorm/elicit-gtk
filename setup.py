60382060e42247ab6694f392ad5d5d7d1cf66896
Brian Mattern <rephorm@rephorm.com>
2010-03-14 23:26:11 -0700

    install .desktop file and icons

--- setup.py
+++ setup.py
@@ -8,7 +8,10 @@ class install_with_schemas(install):
   def run(self):
     install.run(self)
 
-    os.system('gconftool-2 --install-schema-file elicit/data/elicit.schemas')
+    os.system('gconftool-2 --install-schema-file data/elicit.schemas')
+    os.system('xdg-icon-resource install --size 24 data/icon/24/rephorm-elicit.png')
+    os.system('xdg-icon-resource install --size 32 data/icon/32/rephorm-elicit.png')
+    os.system('xdg-desktop-menu install data/rephorm-elicit.desktop')
 
 distutils.core.setup(
     name='elicit',
@@ -20,7 +23,7 @@ distutils.core.setup(
     packages=['elicit'],
     package_data={'elicit': ['data/icons/*.png']},
     scripts=['bin/elicit'],
-    data_files=[('/usr/share/gconf/schemas', ['elicit/data/elicit.schemas'])],
+    data_files=[('/usr/share/gconf/schemas', ['data/elicit.schemas'])],
     cmdclass={'install': install_with_schemas}
     )
 
