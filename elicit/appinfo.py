1195748b6a5a3f3fcf2534939bab07055b1f7ff2
Brian Mattern <rephorm@rephorm.com>
2010-09-28 22:20:22 -0700

    bump version

--- elicit/appinfo.py
+++ elicit/appinfo.py
@@ -5,7 +5,7 @@ dialog and for packaging.
 
 name = 'Elicit'
 pkgname = 'elicit'
-version = '2.0'
+version = '2.1.0'
 description = 'Screen magnifier and color picker',
 website = 'http://www.rephorm.com/code/elicit'
 author = 'Brian Mattern'
